d8a40db4aafd22f9ed9e2af9cb65daed1522d970
wayne <wayne.5540@gmail.com>
2015-12-16 14:05:56 +0800

    add generate spec

--- spec/taiwanese_id_builder_spec.rb
+++ spec/taiwanese_id_builder_spec.rb
@@ -28,6 +28,39 @@ describe TaiwaneseIdBuilder do
         expect( TaiwaneseIdBuilder.valid?(id) ).to be true
       end
     end
+
+    pending "case sensative"
+  end
+
+  describe "#generate" do
+
+    it "always generate valid ID" do
+      fake_ids = 1000.times.map do
+        TaiwaneseIdBuilder.valid?(TaiwaneseIdBuilder.generate)
+      end
+
+      expect(fake_ids.all?{true}).to be true
+    end
+
+    describe "is able to fake male or female ID" do
+      context "when generate female ID" do
+        it "first number charactor should be 2" do
+          fake_id = TaiwaneseIdBuilder.generate("female")
+          first_number = fake_id.split(//)[1]
+
+          expect(first_number).to eq("2")
+        end
+      end
+
+      context "when generate male ID" do
+        it "first number charactor should be 1" do
+          fake_id = TaiwaneseIdBuilder.generate("male")
+          first_number = fake_id.split(//)[1]
+
+          expect(first_number).to eq("1")
+        end
+      end
+    end
   end
 
 end
